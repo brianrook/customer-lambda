0be40c649622cd04341ab9e1f2847eb89bfb346b
brianrook <brian.rook@gmail.com>
2021-07-19 16:39:00 -0600

    store tf state in the cloud

--- tf/main.tf
+++ tf/main.tf
@@ -2,8 +2,45 @@ provider "aws" {
   region     = "${var.aws_region}"
   shared_credentials_file = "/Users/brianrook/.aws/credentials"
 }
-
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "awstraining-terraform-up-and-running-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "awstraining_terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
 data "aws_caller_identity" "current" { }
+
+resource "aws_s3_bucket" "awstraining_terraform_state" {
+  bucket = "awstraining-terraform-up-and-running-state"
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "awstraining_terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 
 resource "aws_iam_role" "iam_for_helloWorld_lambda" {
   name = "iam_for_helloWorld_lambda"
